8296ad61d43ec863464963bf388fc0e7bcc99eb3
MatsLanGoH <mats.lan.pod@googlemail.com>
2017-10-13 13:25:01 +0900

    Add preliminary Book model

--- leselatte/settings.py
+++ leselatte/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'librarian',
 ]
 
 MIDDLEWARE = [
@@ -73,8 +74,11 @@ WSGI_APPLICATION = 'leselatte.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'leselatte_db',
+        'USER': 'leselatte_admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- librarian/models.py
+++ librarian/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateField()
+    page_count = models.IntegerField()
+    authors = ArrayField(models.CharField(max_length=50))
+
