f219792cb9606bca42974f84398d611d5cff06da
MatsLanGoH <mats.lan.pod@googlemail.com>
2017-10-15 00:44:16 +0900

    Add initial template and routing for librarian

--- librarian/views.py
+++ librarian/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+
+from .models import Book
+
 
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'librarian/book_list.html', {'books': books})
